b978371b9d59d538422a62dba9ed795df73a2062
Haoxi Tan <haoxi.tan@gmail.com>
2022-02-14 22:51:36 +1000

    return vals

--- src/lib.rs
+++ src/lib.rs
@@ -11,9 +11,18 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 #[wasm_bindgen]
 extern {
     fn alert(s: &str);
+    // fn console_log_i(num: i32);
+    // fn console_log(s: &str);
+
+}
+
+pub fn some_func() -> i32 {
+    return 42;
 }
 
 #[wasm_bindgen]
-pub fn greet() {
+pub fn greet() -> i32{
     alert("Hello, there!");
+    // console_log_i(some_func());
+    return some_func()
 }
